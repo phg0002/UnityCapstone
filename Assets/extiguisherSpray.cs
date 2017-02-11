95698da487da050f2956605cdeb6d03b383cb331
Perry Garvey <perry.garvey@bellevuecollege.edu>
2017-02-11 14:25:21 -0800

    First commit

---
+++ Assets/extiguisherSpray.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class extiguisherSpray : MonoBehaviour {
+
+    public ParticleSystem spray;
+    public GameObject sprayOrigin;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log(sprayOrigin.transform.position.ToString());
+            ParticleSystem spraying = Instantiate(spray, sprayOrigin.transform);
+            spraying.transform.localPosition = Vector3.zero;
+            spraying.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }    
+    }
+}
